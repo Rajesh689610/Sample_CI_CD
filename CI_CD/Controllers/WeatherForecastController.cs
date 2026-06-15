1b9e57b6026deab32bbb92df7a9c972b10b87b7c
Rajesh689610 <raji689610@gmail.com>
2026-06-15 22:34:57 +0530

    int final testing

--- CI_CD/Controllers/WeatherForecastController.cs
+++ CI_CD/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace CI_CD.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Rajesh","Praneesh"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Rajesh","Praneesh","Kotti","Lithin"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
